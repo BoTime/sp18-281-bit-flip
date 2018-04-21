7cd210f9f3c85e9845f8d835c54ebb1f1fa7cc27
Vimmi Swami <vimmi.swami@outlook.com>
2018-04-21 10:24:03 -0700

    some order related changes for json and ejs

--- routes/order.js
+++ routes/order.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var ejs = require('ejs');
 
+// Return order page
+router.post('/', (req, res) => {
+	console.log("ORDER POST");
+	console.log(req.body);
+	res.render('order');
+});
 // Return order page
 router.get('/', (req, res) => {
 	res.render('order');
 });
 
 module.exports = router;
+ 
