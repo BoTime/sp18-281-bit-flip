44d37313622632b7474304dce4c944efd5f7fa12
Brian Bamsch <bbamsch32@gmail.com>
2018-04-29 08:57:33 -0700

    Add Secondary Lookup for Store
    Add ability to search stores by Name as as secondary identifier.
    
    e.g.
    GET /stores/San%20Jose
    returns a store with the name "San Jose" via lookup by secondary index.

--- apis/src/cmpe281/inventory/handler/handler.go
+++ apis/src/cmpe281/inventory/handler/handler.go
@@ -42,27 +42,12 @@ func (ctx *RequestContext) ListStores(w http.ResponseWriter, r *http.Request) {
 }
 
 func (ctx *RequestContext) GetStore(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-
-	querySelectors := qb.M{
-		"id": nil,
+	store, err := ctx.getStoreById(w, r)
+	if err != nil {
+		store, err = ctx.getStoreByName(w, r)
 	}
 
-	// Parse Query Inputs
-	if storeId, err := gocql.ParseUUID(vars["store_id"]); err == nil {
-		querySelectors["id"] = storeId
-	} else {
-		output.WriteErrorMessage(w, http.StatusBadRequest, "Store ID not provided")
-		return
-	}
-
-	// Set up Query
-	query, names := qb.Select("stores").Where(qb.Eq("id")).ToCql()
-	q := gocqlx.Query(ctx.Cassandra.Query(query), names).BindMap(querySelectors)
-
-	// Execute Query
-	var store model.StoreDetails
-	if err := gocqlx.Iter(q.Query).Get(&store); err != nil {
+	if err != nil {
 		switch err {
 		case gocql.ErrNotFound:
 			output.WriteErrorMessage(w, http.StatusNotFound, "Store not found")
@@ -74,6 +59,46 @@ func (ctx *RequestContext) GetStore(w http.ResponseWriter, r *http.Request) {
 	}
 
 	output.WriteJson(w, store)
+}
+
+func (ctx *RequestContext) getStoreById(w http.ResponseWriter, r *http.Request) (*model.StoreDetails, error) {
+	// Parse UUID
+	storeId, err := gocql.ParseUUID(mux.Vars(r)["store_id"])
+	if err != nil {
+		return nil, err
+	}
+
+	// Set up Query
+	query, names := qb.Select("stores").Where(qb.Eq("id")).Limit(1).ToCql()
+	q := gocqlx.Query(ctx.Cassandra.Query(query), names).BindStruct(model.StoreDetails{
+		Id: storeId,
+	})
+
+	// Execute Query
+	var store model.StoreDetails
+	if err := gocqlx.Get(&store, q.Query); err != nil {
+		return nil, err
+	}
+
+	return &store, nil
+}
+
+func (ctx *RequestContext) getStoreByName(w http.ResponseWriter, r *http.Request) (*model.StoreDetails, error) {
+	// Secondary Index allows us to lookup ID by Store Name
+
+	// Set up Query
+	query, names := qb.Select("stores").Where(qb.Eq("name")).Limit(1).ToCql()
+	q := gocqlx.Query(ctx.Cassandra.Query(query), names).BindStruct(model.StoreDetails{
+		Name: mux.Vars(r)["store_id"],
+	})
+
+	// Execute Query
+	var store model.StoreDetails
+	if err := gocqlx.Get(&store, q.Query); err != nil {
+		return nil, err
+	}
+
+	return &store, nil
 }
 
 func (ctx *RequestContext) GetInventory(w http.ResponseWriter, r *http.Request) {
