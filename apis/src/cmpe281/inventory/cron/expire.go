8c3a8441994699edddb308f9db002ed827c0cb5a
Brian Bamsch <bbamsch32@gmail.com>
2018-05-03 11:11:23 -0700

    Fixes to Allocation Expiration Cron Task
    Expire allocations, need to include the partition keys when doing
    update, so we take the set of unexpired allocations and update them.
    
    Allocations expire after 1 minute, so this will only ever affects a
    handful of allocations at any point in time.

--- apis/src/cmpe281/inventory/cron/expire.go
+++ apis/src/cmpe281/inventory/cron/expire.go
@@ -6,6 +6,7 @@ import (
 	"time"
 	"log"
 	"github.com/gocql/gocql"
+	"cmpe281/inventory/model"
 )
 
 type ShardedDatabaseContext struct {
@@ -22,28 +23,63 @@ func (ctx *CleanerContext) Cleanup() {
 	// TODO(bbamsch): Restore inventory
 
 	log.Println("Cleanup Started...")
+	expireTime := time.Now()
 
-	query, names := qb.Update("allocations").SetNamed("status", "updated_status").Where(qb.Eq("status"), qb.Lt("expires")).ToCql()
-	q := gocqlx.Query(ctx.Database.Shard1.Query(query), names).BindMap(qb.M{
-		"updated_status": "Expired",
-		"status": []string{"Unconfirmed"},
-		"expires": time.Now(),
-	})
+	query, names := qb.Select("allocations").
+		Columns("store_id", "user_id", "id", "expires").
+			Where(qb.Eq("status")).ToCql()
+	allocs := make([]model.AllocationDetails, 0)
+	q := gocqlx.Query(ctx.Database.Shard1.Query(query), names)
+	if err := gocqlx.Select(&allocs, q.Query); err == nil {
+		batch := qb.Batch()
+		batchData := qb.M{}
+		for _, alloc := range allocs {
+			prefix := alloc.Id.String()
 
-	if err := q.ExecRelease(); err != nil {
-		log.Println("Failed to expire allocations from Shard1")
-		return
+			batch.AddWithPrefix(prefix, qb.Update("allocations").SetNamed("status", "updated_status").
+				Where(qb.Eq("store_id"), qb.Eq("user_id"), qb.Eq("id")).
+				If(qb.Eq("status"), qb.Lt("expires")))
+
+			batchData[prefix + ".store_id"] = alloc.StoreId
+			batchData[prefix + ".user_id"] = alloc.UserId
+			batchData[prefix + ".id"] = alloc.Id
+			batchData[prefix + ".expires"] = expireTime
+			batchData[prefix + ".updated_status"] = "Expired"
+			batchData[prefix + ".status"] = "Unconfirmed"
+		}
+
+		updateQuery, updateNames := batch.ToCql()
+		q = gocqlx.Query(ctx.Database.Shard1.Query(updateQuery), updateNames).BindMap(batchData)
+		if err := q.ExecRelease(); err != nil {
+			log.Println("Failed to expire allocations in Shard1")
+		}
 	}
 
-	q = gocqlx.Query(ctx.Database.Shard2.Query(query), names).BindMap(qb.M{
-		"updated_status": "Expired",
-		"status": []string{"Unconfirmed"},
-		"expires": time.Now(),
-	})
+	allocs = make([]model.AllocationDetails, 0)
+	q = gocqlx.Query(ctx.Database.Shard2.Query(query), names)
+	if err := gocqlx.Select(&allocs, q.Query); err == nil {
+		batch := qb.Batch()
+		batchData := qb.M{}
+		for _, alloc := range allocs {
+			prefix := alloc.Id.String()
 
-	if err := q.ExecRelease(); err != nil {
-		log.Println("Failed to expire allocations from Shard2")
-		return
+			batch.AddWithPrefix(prefix, qb.Update("allocations").SetNamed("status", "updated_status").
+				Where(qb.Eq("store_id"), qb.Eq("user_id"), qb.Eq("id")).
+				If(qb.Eq("status"), qb.Lt("expires")))
+
+			batchData[prefix + ".store_id"] = alloc.StoreId
+			batchData[prefix + ".user_id"] = alloc.UserId
+			batchData[prefix + ".id"] = alloc.Id
+			batchData[prefix + ".expires"] = expireTime
+			batchData[prefix + ".updated_status"] = "Expired"
+			batchData[prefix + ".status"] = "Unconfirmed"
+		}
+
+		updateQuery, updateNames := batch.ToCql()
+		q = gocqlx.Query(ctx.Database.Shard2.Query(updateQuery), updateNames).BindMap(batchData)
+		if err := q.ExecRelease(); err != nil {
+			log.Println("Failed to expire allocations in Shard2")
+		}
 	}
 
 	log.Println("Cleaned Finished!")
