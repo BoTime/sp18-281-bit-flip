7cc3f5efd051840270a0f735a9ea63672fecbb7b
Ubuntu <ubuntu@ip-10-0-1-205.us-west-1.compute.internal>
2018-05-03 05:40:06 +0000

    Update Ip Echo Back for EC2

--- apis/src/cmpe281/user/handlers/health_check_handler.go
+++ apis/src/cmpe281/user/handlers/health_check_handler.go
@@ -18,16 +18,16 @@ func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
         panic(err)
     }
 
-    for _, iface := range list {
-        // fmt.Printf("%d name=%s %v\n", i, iface.Name, iface)
-        if iface.Name == "en0" {
+    for i, iface := range list {
+        fmt.Printf("%d name=%s %v\n", i, iface.Name, iface)
+        if iface.Name == "eth0" {
             addrs, err := iface.Addrs()
             if err != nil {
                 panic(err)
             }
             for j, ip_address := range addrs {
                 fmt.Printf(" %d %v\n", j, ip_address.String())
-                if j == 1 {
+                if j == 0 {
                     health_check_msg.WriteString(ip_address.String())
                 }
             }
