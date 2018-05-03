a8f59c3a741482a88f38d91cfc33b79011e9c16a
botime <bo.liu@sjsu.edu>
2018-05-02 22:31:18 -0700

    Add Ip address to User API Health Check

--- apis/src/cmpe281/user/handlers/health_check_handler.go
+++ apis/src/cmpe281/user/handlers/health_check_handler.go
@@ -2,10 +2,37 @@ package handlers
 
 import (
     "net/http"
+    "net"
+    "bytes"
     "fmt"
 )
 
 func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
-    fmt.Fprintf(w, "Starbucks User API V1.0 - Bo")
+
+    var health_check_msg bytes.Buffer
+    health_check_msg.WriteString("Starbucks User API V1.0 - Bo - ")
+
+    list, err := net.Interfaces()
+    if err != nil {
+        panic(err)
+    }
+
+    for _, iface := range list {
+        // fmt.Printf("%d name=%s %v\n", i, iface.Name, iface)
+        if iface.Name == "en0" {
+            addrs, err := iface.Addrs()
+            if err != nil {
+                panic(err)
+            }
+            for j, ip_address := range addrs {
+                fmt.Printf(" %d %v\n", j, ip_address.String())
+                if j == 1 {
+                    health_check_msg.WriteString(ip_address.String())
+                }
+            }
+        }
+    }
+
+    fmt.Fprintf(w, health_check_msg.String())
 }
